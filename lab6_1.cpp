2e1f38f76ae25a12050cc95a4a3d945f71bc5ed8
TRex612 <poochit.moonkaew@gmail.com>
2022-12-09 15:58:41 +0700

    cp

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,28 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int i = 1 ;
+    int j = 0 ;
+    int N[1000] ;
+    int evennum = -1 ;
+    int oddnum = 0 ;
+    while (i != 0)
+    {
+        int itg ;
+        cout << "Enter an integer: ";
+        cin >> itg ;
+        N[j] = itg ;
+        if ((N[j])%2 == 0) {
+            evennum++ ;
+        }
+        else{
+            oddnum++;
+        }
+        i = itg ;
+        j++;    
+    }
+    cout << "#Even numbers = " << evennum ;
+    cout << "\n" ;
+    cout << "#Odd numbers = " << oddnum ;
     return 0;
-}
+    }
